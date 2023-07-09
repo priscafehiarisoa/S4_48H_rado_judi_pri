a1b92315bb26c969cf6ac3457f9006183dc0e6ea
priscafehiarisoa <mytyrealy@gmail.com>
2023-07-09 21:26:25 +0300

    feat : initialisation ajout de codeigniter

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!is_dir('./uploads/')) {
+            mkdir('./uploads/', 0777, true);
+        }
+    }
+
+    public function index(){$this->load->view('welcome_message');}
+
+
+}
+
