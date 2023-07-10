ee9cdbb8838c376a5120f86aed0ea7d3edef4189
priscafehiarisoa <mytyrealy@gmail.com>
2023-07-10 06:11:36 +0300

    feat : initialisation ajout template

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/takalo5/';
+$config['base_url'] = 'http://localhost/S4_48H_rado_judi_pri/';
 
 $config['index_page'] = 'index.php';
 
