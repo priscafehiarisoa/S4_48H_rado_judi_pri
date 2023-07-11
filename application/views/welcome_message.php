36c0638359c2dffdf66882784ffd459ffe822b14
priscafehiarisoa <mytyrealy@gmail.com>
2023-07-11 11:11:26 +0300

    rearrangement de code affichage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,1 +1,13 @@
-<?php ?>
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+include "inc/root.php" ?>
+
+<main class="main-content p-2">
+    <?php include "inc/up_navbar.php";?>
+
+    <div class="container">
+        <h1>welcome page client </h1>
+    </div>
+</main>
